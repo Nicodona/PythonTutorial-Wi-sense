fa01a0845257a2f65e189420669029ddde04a0ce
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-04-09 08:51:05 +0100

    Add files via upload
    lesson2 python tutorials. printing text

---
+++ display.py_lesson2.py
@@ -0,0 +1,22 @@
+print('''There once was a movie star icon
+who preferred to sleep with the light on.
+They learned how to code
+a device that sure glowed
+and lit up the night using Python!''')
+
+#skipping 3 lines
+print('\n\n\n')
+
+# you can also do this by using \n
+
+print('There once was a movie star icon\n who preferred to sleep with the light on.\nThey learned how to code\n device that sure glowed\nand lit up the night using Python!')
+
+#skipping 2 lines
+print('\n\n')
+
+# another way of doing this is by using the print function several lines
+print('There once was a movie star icon')
+print('who preferred to sleep with the light on.')
+print('They learned how to code')
+print('a device that sure glowed')
+print('and lit up the night using Python!')
