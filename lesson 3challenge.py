203eee8e86dcf9629b998dedc19d828573c3a70e
Nicodona <71098788+Nicodona@users.noreply.github.com>
2021-04-11 21:55:11 +0100

    Add files via upload
    solution to lesson 3

---
+++ lesson 3challenge.py
@@ -0,0 +1,12 @@
+## this program code creates a short tory about individuals when they provide their names age and favourites
+
+
+name = input('enter your name ')
+age = input('enter your age ')
+name = name.capitalize()
+pet = input('enter best pet ')
+pet = pet.capitalize()
+fav = input('enter favourite color ')
+fav = fav.capitalize()
+
+print('My name is '+name+',  I am '+age+' years old. I love '+pet+' and my favourite color is '+fav)
